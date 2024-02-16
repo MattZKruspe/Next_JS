6d55dcb8c0a72353b75d2890713d63a2a2b309e4
MattZKruspe <matthewmarquez2003@gmail.com>
2024-02-16 13:37:12 -0600

    2do Commit Cap3

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,5 @@
-import { Inter, Lusitana } from 'next/font/google';
+import { Inter, Lusitana, Arima } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 export const Lusi = Lusitana({ subsets: ['latin'], weight: '700' });
+export const Ari = Arima ({ subsets: ['latin'], weight:'700'});
