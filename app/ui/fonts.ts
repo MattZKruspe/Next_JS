8abd18f344ac8ee1d770ff8fa6f28ff8e5b44245
MattZKruspe <matthewmarquez2003@gmail.com>
2024-02-19 12:47:04 -0600

    Correciones

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,10 @@
-import { Inter, Lusitana, Arima } from 'next/font/google';
+import { Inter, Lusitana} from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
-export const Lusi = Lusitana({ subsets: ['latin'], weight: '700' });
+
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: ['700', '700'] 
+
+});
 
